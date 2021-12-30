a19267416a660749c3740ce961a00ce9e349718c
Citromszorny <bence5605@gmail.com>
2021-12-30 13:55:07 +0100

    Pálya és a csigák kinézete

--- masodikProjektKomplex/masodikProjektKomplex/MainWindow.xaml.cs
+++ masodikProjektKomplex/masodikProjektKomplex/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
+
 
 namespace masodikProjektKomplex
 {
@@ -23,6 +25,30 @@ namespace masodikProjektKomplex
         public MainWindow()
         {
             InitializeComponent();
+            csiga1.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(@"csiga1.png", UriKind.Relative))
+            };
+            csiga2.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(@"csiga2.png", UriKind.Relative))
+            };
+            csiga3.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(@"csiga3.png", UriKind.Relative))
+            };
+            elsoPalya.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(@"elsoPalya.png", UriKind.Relative))
+            };
+            masodikPalya.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(@"masodikPalya.png", UriKind.Relative))
+            };
+            harmadikPalya.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(@"harmadikPalya.png", UriKind.Relative))
+            };
         }
     }
 }
